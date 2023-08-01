68dfe2838fd632e2b47c788e2d82c3ec92d79256
fifa <fifaatade2020@gmail.com>
2023-08-01 19:26:12 +0100

    first commit

---
+++ src/router/index.ts
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import ToDo from '@/views/ToDo.vue'
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+    path: '/',
+    name: 'home',
+    component: ToDo
+  }
+  ]
+})
+
+export default router
---
+++ src/views/ToDo.vue
@@ -0,0 +1,95 @@
+<template>
+    <!--<form>
+        <h1>TO-DO-LIST</h1>
+        <div class="to-do-list">
+            <input type="checkbox" class="list">
+            <label for="task1">lessive</label>
+        </div>
+        <div class="to-do-list">
+            <input type="checkbox" class="list">
+            <label for="task2">cuisine</label>
+        </div>
+    </form>!-->
+    <!--<div :class="element.is_finished?'color':''" v-for="element in tab">  pour mettre une condition si et sinon --> 
+        <div v-bind:class="{'color barre':element.is_finished}"  v-for="element in tab">
+        <div v-if="element.is_finished">
+            <p class="barre"> {{ element.name }}</p>
+            <input type="checkbox"  :checked="element.is_finished" name="" id="" >
+        </div>
+        <div v-else>
+            <p > {{ element.name }}</p>
+            <input type="checkbox"  :checked="element.is_finished" name="" id="" >
+            <div id="task">
+                <label for="task"> Quelle tache voulez-vous ajouter?</label>
+                <input type="text" v-model="name">
+                <button @click="tache()"> soumettre</button>
+                <p class="barre"> {{ name }}</p>
+                <input type="checkbox" name="" id="" >
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { METHODS } from 'http';
+import { VueElement } from 'vue';
+
+import {ref} from 'vue'
+const count =ref(0)
+let tab=[
+    {
+        name:'lessive',
+        is_finished:true
+    },
+    {
+        name:'cuisine',
+        is_finished:false
+    },
+    {
+        name:'lessive',
+        is_finished:true
+    },
+    {
+        name:'cuisine',
+        is_finished:false
+    }
+ ]
+new Vue{
+    el:'#task',
+    Data:{
+        name:''
+    },
+    METHODS:{
+        tache(){
+            alert(this.name);
+        }
+    }
+})
+
+</script>
+
+<style scoped>
+    .color{
+        border: 1px solid green;
+    }
+    .barre{
+        text-decoration: line-through;
+    }
+    h1{
+        text-align: center;
+        color: #ddd;
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        font-size: 40px;
+    }
+    form{
+        max-width: 75%;
+        margin: 0 auto;
+        border: 1px solid #eee;
+        border-radius: 8px;
+    }
+    .to-do-list{
+        max-width: 80%;
+        margin: 0 auto;
+    }
+</style>
+
